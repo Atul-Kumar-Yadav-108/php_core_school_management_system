372ee7a8e0b913fd61897625d7ee8f2e2eeb70e7
Atul Kumar Yadav <atulanirudhyadav@gmail.com>
2026-05-24 17:55:41 +0530

    class list and delete feature added

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,45 @@ console.log("School Management System Loaded 🚀");
 function confirmDelete() {
   return confirm("Are you sure you want to delete this record?");
 }
+
+$(document).on("click", ".deleteBtn", function () {
+  let id = $(this).data("id");
+  let table = $(this).data("table");
+  Swal.fire({
+    title: "Are you sure?",
+    text: "This record will be moved to trash!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#d33",
+    cancelButtonColor: "#3085d6",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      $.ajax({
+        url: "../../php_core_school_management_system/controllers/AjaxController.php",
+        type: "POST",
+        data: {
+          id: id,
+          table: table,
+          action: "delete_action",
+        },
+        success: function (response) {
+          console.log("SERVER RESPONSE:", response);
+
+          Swal.fire({
+            title: "Deleted!",
+            text: response,
+            icon: "success",
+            timer: 1500,
+            showConfirmButton: false,
+          });
+
+          location.reload();
+        },
+        error: function (xhr) {
+          console.log("AJAX ERROR:", xhr.responseText);
+        },
+      });
+    }
+  });
+});
---
+++ controllers/AjaxController.php
@@ -0,0 +1,80 @@
+<?php
+include('../helpers/helpers.php');
+include('../models/AjaxModel.php');
+session_start();
+include("../config/database.php");
+
+if (isset($_POST['action']) && $_POST['action'] == 'class_list') {
+    // errorPrint("Checking data");
+    // get_all_classes();
+    // DataTables params
+    $draw  = $_POST['draw'];
+    $start = $_POST['start'];
+    $length = $_POST['length'];
+
+    $search = $_POST['search']['value'];
+
+    // Sorting
+    $columnIndex = $_POST['order'][0]['column'];
+
+    $orderBy = $_POST['columns'][$columnIndex]['data'] ?? 'id';
+
+    $columnDir = $_POST['order'][0]['dir'];
+
+    $response = get_all_classes($con, $start, $length, $search, $orderBy, $columnDir);
+    $tableData = [];
+    // errorPrint($response['data']);
+    if (!empty($response['data'])) {
+        $srl = $start;
+
+        foreach ($response['data'] as $row) {
+
+            $srl++;
+
+            $tableData[] = [
+
+                "srl" => $srl,
+
+                "class_name" => $row['class_name'],
+
+                "created_on" => date(
+                    "d M Y",
+                    strtotime($row['created_on'])
+                ),
+
+                "status" => $row['status'] == 1
+                    ? '<span class="badge bg-success">Active</span>'
+                    : '<span class="badge bg-danger">Inactive</span>',
+
+                "action" => '
+                <a class="btn btn-sm btn-primary editBtn"
+                    href="index.php?page=add_classes&id=' . $row['id'] . '">
+                    Edit
+                </a>
+
+                <button class="btn btn-sm btn-danger deleteBtn"
+                    data-id="' . $row['id'] . '" data-table="tbl_classes_masters">
+                    Delete
+                </button>
+            '
+            ];
+        }
+    }
+    echo json_encode([
+        "draw" => intval($draw),
+        "recordsTotal" => $response['totalRecords'],
+        "recordsFiltered" => $response['filteredRecords'],
+        "data" => $tableData
+    ]);
+}
+
+
+
+// global delete
+
+if (isset($_POST['action']) && $_POST['action'] == 'delete_action') {
+    $id = $_POST['id'];
+    $table = $_POST['table'];
+    delete($con, $id, $table); // no return
+    exit;
+}
