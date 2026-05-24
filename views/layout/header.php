ad39efb153abcd1c06cf1858b744549bf7eccae8
Atul Kumar Yadav <atulanirudhyadav@gmail.com>
2026-05-24 14:23:22 +0530

    changes made as change the location for projects

--- views/layout/header.php
+++ views/layout/header.php
@@ -19,7 +19,7 @@ include('./helpers/helpers.php');
     <!-- jquery validate script  -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.21.0/jquery.validate.min.js" integrity="sha512-KFHXdr2oObHKI9w4Hv1XPKc898mE4kgYx58oqsc/JqqdLMDI4YjOLzom+EMlW8HFUd0QfjfAvxSL6sEq/a42fQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
-    <link rel="stylesheet" href="../../PHPPractices\schoolmanagementsystem\assets\css\style.css">
+    <link rel="stylesheet" href="../../php_core_school_management_system/assets/css/style.css">
 
     <style>
         body {
